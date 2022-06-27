53565b3398fe343b72512a1eecbb66d7d9e6129f
luis <linkinluisave@gmail.com>
2022-06-26 23:06:51 -0500

    update libraries room, navigation and hilt... change some clases by  abstract implementation

--- app/src/main/java/com/tp/testrap/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/tp/testrap/repository/MovieRepositoryImpl.kt
@@ -1,10 +1,10 @@
 package com.tp.testrap.repository
 
 import com.tp.testrap.core.InternetCheck
-import com.tp.testrap.data.local.LocalMovieDataSourceImpl
+import com.tp.testrap.data.local.LocalMovieDataSource
 import com.tp.testrap.data.mappers.MoviesMapper
 import com.tp.testrap.data.model.MovieList
-import com.tp.testrap.data.remote.RemoteMovieDataSourceImpl
+import com.tp.testrap.data.remote.RemoteMovieDataSource
 import javax.inject.Inject
 
 enum class TypeListMovie constructor(val type: String) {
@@ -14,40 +14,40 @@ enum class TypeListMovie constructor(val type: String) {
 }
 
 class MovieRepositoryImpl @Inject constructor(
-    private val dataSourceRemoteImpl: RemoteMovieDataSourceImpl,
-    private val dataSourceLocalImpl: LocalMovieDataSourceImpl
+    private val dataSourceRemote: RemoteMovieDataSource,
+    private val dataSourceLocal: LocalMovieDataSource
 ) : MovieRepository {
 
     override suspend fun getUpcomingMovies(): MovieList {
         return if (InternetCheck.isNetWorkAvailable()) {
             val listLocal =
-                MoviesMapper(TypeListMovie.UP_COMMING.type).map(dataSourceRemoteImpl.getUpCommingMovies().results)
-            dataSourceLocalImpl.saveListMovie(listLocal)
-            dataSourceLocalImpl.getUpCommingMovies()
+                MoviesMapper(TypeListMovie.UP_COMMING.type).map(dataSourceRemote.getUpCommingMovies().results)
+            dataSourceLocal.saveListMovie(listLocal)
+            dataSourceLocal.getUpCommingMovies()
         } else {
-            dataSourceLocalImpl.getUpCommingMovies()
+            dataSourceLocal.getUpCommingMovies()
         }
     }
 
     override suspend fun getToRatedMovies(): MovieList {
         return if (InternetCheck.isNetWorkAvailable()) {
             val listLocal =
-                MoviesMapper(TypeListMovie.TOP_RATED.type).map(dataSourceRemoteImpl.getTopRatedMovies().results)
-            dataSourceLocalImpl.saveListMovie(listLocal)
-            dataSourceLocalImpl.getTopRatedMovies()
+                MoviesMapper(TypeListMovie.TOP_RATED.type).map(dataSourceRemote.getTopRatedMovies().results)
+            dataSourceLocal.saveListMovie(listLocal)
+            dataSourceLocal.getTopRatedMovies()
         } else {
-            dataSourceLocalImpl.getTopRatedMovies()
+            dataSourceLocal.getTopRatedMovies()
         }
     }
 
     override suspend fun getPopularMovies(): MovieList {
         return if (InternetCheck.isNetWorkAvailable()) {
             val listLocal =
-                MoviesMapper(TypeListMovie.POPULAR.type).map(dataSourceRemoteImpl.getPopularMovies().results)
-            dataSourceLocalImpl.saveListMovie(listLocal)
-            dataSourceLocalImpl.getPopularMovies()
+                MoviesMapper(TypeListMovie.POPULAR.type).map(dataSourceRemote.getPopularMovies().results)
+            dataSourceLocal.saveListMovie(listLocal)
+            dataSourceLocal.getPopularMovies()
         } else {
-            dataSourceLocalImpl.getPopularMovies()
+            dataSourceLocal.getPopularMovies()
         }
     }
 }
